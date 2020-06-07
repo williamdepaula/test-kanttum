761260e3a215386dbffe63b788eb2b29a0691215
William de Paula <williampereiradepaula@gmail.com>
2020-06-07 03:26:06 -0400

    Adição dos testes de notificacoes

--- src/components/notificacoes/notificacoes.vue
+++ src/components/notificacoes/notificacoes.vue
@@ -11,7 +11,7 @@
 
 <script>
 export default {
-    
+    name: 'app-notificacao'
 }
 </script>
 
---
+++ tests/unit/components/notificacoes.spec.js
@@ -0,0 +1,28 @@
+import { mount, shallowMount } from '@vue/test-utils';
+import AppNotificacao from '@/components/notificacoes/notificacoes.vue';
+
+describe('App está montada', () => {
+    const wrapper = mount(AppNotificacao);
+
+    test('é uma instancia Vue', () => {
+        expect(wrapper).toBeTruthy();
+    });
+
+    test('está renderizando corretamente', () => {
+        const notificacao_vazia = "<div class=\"notificacoes\"><a href=\"#\"><img src=\"src/assets/icon-notifications.svg\" alt=\"\"> <span class=\"badge badge-danger\"></span></a></div>";
+        expect(wrapper.html()).toEqual(notificacao_vazia);
+    });
+
+    test('está exibindo 3 quando 3 é passado', () => {
+
+        const shallowWrapper = shallowMount(AppNotificacao,{
+            scopedSlots: {
+                default: '<app-notificacao>3</app-notificacao>'
+            }
+        });
+
+        expect(shallowWrapper.html()).toContain('3')
+        console.log("Teste: ", shallowWrapper.html());
+    });
+    
+});
